cc9acfa1160145458393e721bbfc440f4d5726c0
KrivutsenkoVD <xeokoin@gmail.com>
2023-05-23 16:18:30 +0300

    added homework3

--- lecture3/hw3.py
+++ lecture3/hw3.py
@@ -2,61 +2,40 @@ class Array(object):
     def __init__(self, *args):
         self._data = [*args]
 
-    # Добавляет элемент в конец списка
-    def add(self, item):
-        self._data.append(item)
+    def __getitem__(self, item):
+        return self._data[item]
+
+    def __len__(self):
+        return len(self._data)
+
+    def __add__(self, other):
+        return self._data + other._data
+
+    def append(self, item):
+        self._data += [item]
         return self._data
 
-    # Получаем index элемента, если нет указанного элемента возвращаем -1
-    def index(self, item):
+    def index(self, elem):
         try:
-            _ind = self._data.index(item)
-            return _ind
+            return self._data.index(elem)
         except ValueError:
             return -1
 
-    # Получаем длину
-    def len(self):
-        return len(self._data)
 
-    # Возвращаем весь список
-    def result(self, index=None):
-        return self._data
-
-    # Поиск элемента по индексу, с валидацией существования указанного индекса,
-    # типа аргумента (int) и наличие самого аргумента
-    def elem_by_index(self, index=None):
-        if index is not None:
-            try:
-                return self._data[index]
-            except IndexError:
-                return 'Элемента с таким индексом не существует!'
-            except TypeError:
-                return 'Индекс может быть только int!'
-        else:
-            return 'Нет обязательного аргумента!'
+arr00 = Array()
+arr01 = Array(1, 2, 3)
 
 
-# Создание
-arr01 = Array()
-arr02 = Array(1)
-arr03 = Array(1, 2, 3)
-
-# Добавление
-print('Результат добавление объекта к классу: {0}'.format(arr02.add(42)))
-
-# Длина
-print('Длина: {0}'.format(arr02.len()))
-
-# Сложение
-print('Сложение: {0}'.format(arr02.result() + arr03.result()))
-
-# Index объекта
-print('Индекс объекта: {0}'.format(arr02.index(5)))
-
-# Цикл
-for i in arr02.result():
+# element by index
+print(arr01[2])
+# len
+print(len(arr01))
+# сложение
+print(arr00 + arr01)
+# append
+print(arr00.append(5))
+# работа с циклами
+for i in arr01:
     print(i)
-
-# Получать значение по индексу
-print('Получение значения по индексу: {0}'.format(arr02.elem_by_index()))
+# index by element
+print(arr01.index(2))
